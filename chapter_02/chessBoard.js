42a24d92afec418c3ac4cbc3f6fa575b0c052ac8
allen alejandro <allenalejandro3@gmail.com>
2021-06-23 10:53:19 -0500

    chapter 2 exercises

---
+++ chapter_02/chessBoard.js
@@ -0,0 +1,44 @@
+/*
+Chessboard
+Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+
+Passing this string to console.log should show something like this:
+
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+When you have a program that generates this pattern, define a binding size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
+*/
+
+
+
+
+
+// const chessBoard = (length, width) => {
+//   let wide = ' # # # #';
+//   let heigth = '# # # #';
+
+  
+// }
+
+
+
+
+
+const chessBoard = size => {
+  let board = "";
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      (j + i) % 2 == 0 ? board += " " : board += "#";
+    }
+    board += "\n";
+  }
+  return board;
+}
+
+console.log(chessBoard(9));
